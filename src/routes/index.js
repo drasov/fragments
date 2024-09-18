3d4b97e355bfdf455ae441f5982fe3be53be08f5
Your Namgit config --global user.email <drasov@myseneca.ca>
2024-09-18 17:42:20 -0400

    Refactored app, src, and route/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,8 @@ const { version, author } = require('../../package.json');
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+const { createSuccessResponse } = require('../response'); 
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
@@ -24,13 +26,12 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
+  res.status(200).json(createSuccessResponse({
     author,
     // Use your own GitHub URL for this!
     githubUrl: 'https://github.com/drasov/fragments',
     version,
-  });
+  }));
 });
 
 module.exports = router;
